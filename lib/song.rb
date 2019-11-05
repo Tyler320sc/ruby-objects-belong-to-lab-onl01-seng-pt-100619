2c8ef587459bfc978c0178a0b1089ddc15c2cf1d
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-05 04:09:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,11 @@
 class Song
   attr_accessor :title, :artist
   
-  
-  
   # def initialize(title)
   #   @title = title
   # end 
   
-def artist_name
-  artist.name if artist
-end
-  
+  def artist_name
+    artist.name if artist
+  end
 end 
