94101b5a4ab8b66ce7a170e71e3444de8d2ffde7
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-05 04:27:29 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,16 @@
 class Post
   attr_accessor :title, :author_name
   
-  @@all = []
+  # @@all = []
   
   # def initialize(title)
   #   @title = title
   #   @@all << self 
   # end 
   
-  def self.all
-    @@all 
-  end
+  # def self.all
+  #   @@all 
+  # end
   
   def author_name
     author.name if author
